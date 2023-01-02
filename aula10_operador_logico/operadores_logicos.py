1eb53329ac30613c8223fbec2c98de208dbf5f72
Arlyjr <arly.lcj@gmail.com>
2023-01-02 15:26:59 -0300

    operador logico atualizado

--- aula10_operador_logico/operadores_logicos.py
+++ aula10_operador_logico/operadores_logicos.py
@@ -1,6 +1,6 @@
 #São operadores utilizados em conjunto com os operadores de comparação, para montar uma expressão lógica. Quando um operador de comparação é utilizado, o resultado retornado é um booleano, dessa forma podemos combinar operadores de comparação com os operadores lógicos, exemplo: op_comparacao + op_logico + op_comparacao… N'
 # AND = Para ser True tudo tem que ser True 
-# OR = Para ser True apena um tem que ser True
+# OR = Para ser True apena um tem que ser True.
 print(True and True) 
 print(True and False) 
 print(False and False) 
