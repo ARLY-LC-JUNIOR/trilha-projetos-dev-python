3f4d628e0fc6dc738e33efc02e5efef14c29352b
Arlyjr <arly.lcj@gmail.com>
2023-03-11 02:29:34 -0300

    manipulação de string string_fatiamento

---
+++ aula15_gerenciador-de-senhas/aula16_string_fatiamento/string1.py
@@ -0,0 +1,21 @@
+nome = "Arly jUNiOr"
+
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = "     Olá Mundo cheguei com python     "
+
+print(texto + ".")
+print(texto.strip() + ".")
+print(texto.rstrip() +".")
+print(texto.lstrip() +".")   
+
+menu = "Python"
+
+print("####" + menu + "####")
+print(menu.center(14))
+print(menu.center(14,"#"))
+print("P-y-t-h-o-n")
+print("-". join(menu))
