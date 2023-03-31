8048a7fbf72ba0a314d9692c7b9fb157715f29e4
Arlyjr <arly.lcj@gmail.com>
2023-03-31 01:44:51 -0300

    interpretação de variáveis

---
+++ aula_16_old_style/string_1.py
@@ -0,0 +1,20 @@
+nome = "Arly Junior"
+idade = 50
+profissão = "Analista"
+linguagem = "Python"
+saldo = 45.435
+
+dados = {"nome": "Arly Junior", "idade": 50} 
+
+print ("Nome: %s Idade: %d" % (nome, idade))
+
+print ("Nome: {} Idade: {}".format (nome, idade))
+print ("Nome: {1} Idade: {0}".format (idade, nome))
+print ("Nome: {1} Idade: {0} Nome: {1} {1}".format (idade, nome))
+print ("Nome: {nome} Idade: {idade}".format(nome=nome, idade=idade))
+print ("Nome: {name} Idade: {age} {name} {name} {age}".format(age=idade, name=nome))
+print ("Nome: {nome} Idade: {idade}".format(**dados))
+
+print(f"Nome: {nome} Idade: {idade}")
+print(f"Nome: {nome} Idade: {idade} Saldo: {saldo:10.2f}")
+
